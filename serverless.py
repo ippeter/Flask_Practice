51be33a926140caf1071ff8646bb4958fb15de23
Peter Predtechensky <peterp@Peters-MacBook-Pro.local>
2020-03-11 11:57:13 +0300

    Accept picture

--- serverless.py
+++ serverless.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import request
+from werkzeug.utils import secure_filename
+
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Index Page"
+@app.route('/picture', methods=['GET', 'POST'])
+def picture():
+    if (request.method == 'POST'):
+        print(request.files)
+        file = request.files['data']
+        img_str = file.read()
+        nparr = np.fromstring(img_str, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+        print(img_np.shape)
+    return "Image processed."
 
 @app.route('/simple', methods=['GET', 'POST'])
 def simple():
